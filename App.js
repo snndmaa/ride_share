18a8249ed573a6bb3a1a3e2cae0703e39eb4c720
sndmaa <sanclaw55@gmail.com>
2023-02-15 22:34:47 +0400

    first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView, Platform } from 'react-native'
+import { Provider } from 'react-redux'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { store } from './store'
+import HomeScreen from './screens/HomeScreen'
+import MapScreen from './screens/MapScreen'
+
+export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
+  return (
+
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={ Platform.OS === 'ios' ? 'padding' : 'height' }
+            style={{ flex:1 }}
+            keyboardVerticalOffset={ Platform.OS === 'ios' ? -64 : 0 }
+          >
+            <Stack.Navigator>
+              <Stack.Screen 
+                name='HomeScreen' 
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen 
+                name='MapScreen' 
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
---
+++ screens/MapScreen.js
@@ -0,0 +1,55 @@
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useNavigation } from '@react-navigation/native'
+
+import Map from '../components/Map'
+import NavigateCard from '../components/cards/NavigateCard'
+import RideOptionsCard from '../components/cards/RideOptionsCard'
+
+
+const MapScreen = () => {
+
+  const Stack = createNativeStackNavigator()
+  const navigation = useNavigation()
+
+  return (
+    <View>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('HomeScreen')}
+        style={ tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg` }
+      >
+
+      </TouchableOpacity>
+
+      <View style={ tw`h-1/2` }>
+        <Map/>
+      </View>
+
+      <View style={ tw`h-1/2` }>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='NavigateCard'
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='RideOptionsCard'
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
